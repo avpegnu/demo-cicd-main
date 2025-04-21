845648043fc1142a70b031cafc2fe4cbdc947579
avpegnu <vietanh142004@gmail.com>
2025-04-21 17:05:11 +0700

    fix

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
+require("dotenv").config();
+
 const express = require("express");
-const router = express.Router();
+const app = express();
 
-router.use("/student", require("./student"));
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
 
-module.exports = router;
+const cors = require("cors");
+app.use(cors());
+
+app.use("/api", require("./routes"));
+
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => console.log(`App listening on port ${PORT}!`));
